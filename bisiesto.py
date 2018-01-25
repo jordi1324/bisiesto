a9775bbc4b2ea62922d9088ef49b8f3a9ede8135
jordi1324 <jordigarciaeguren@outlook.es>
2018-01-25 13:37:43 +0100

    first commit

--- bisiesto.py
+++ bisiesto.py
@@ -1,8 +1,45 @@
-anio=int(input("Escriu un anio: "))
+def esbisiesto (anio):
+    if  anio%4==0 and anio%100!=0 or anio%400==0:
+        print("Bisiesto")
+    else:
+        print("Normal")
 
-if  anio%4==0 and anio%100!=0 or anio%400==0:
-    print("Bisiesto")
-else:
-    print("Normal")
+print("Hey")
+
+esbisiesto(2100)
+esbisiesto(2300)
+esbisiesto(2000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
